49a54d3680a4bdf1c4bb91d3d1d55bdab7bbcd3c
Igor Shevchenko <cyberskunk@gmail.com>
2025-07-01 23:53:53 +0300

    Decrease score size

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -8,7 +8,7 @@ interface ScoreDisplayProps {
 const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ score, maxScore }) => {
   return (
     <div className="relative">
-      <div className="text-white text-9xl font-bold w-36 text-center">{score}</div>
+      <div className="text-white text-7xl font-bold w-32 text-center">{score}</div>
       <div className="text-gray-500 text-xs absolute top-0 right-0 mt-1 mr-1">/{maxScore}</div>
     </div>
   );
